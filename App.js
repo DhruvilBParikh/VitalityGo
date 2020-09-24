aa344d031f058f7ac5be4d386286b697811059ae
DhruvilBParikh <parikh.dhruvil18@gmail.com>
2020-09-23 23:23:19 -0700

    Get Started Screen

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar'
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import GetStarted from './screens/GetStarted'
+import Colors from './constants/colors'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <GetStarted />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container:{
+    height:'100%',
+    width:'100%'
+  }
 });
