29e85f44fce476584dabfd93e3b7726b16608286
Shivang Mistry <shivangmistry96@gmail.com>
2020-09-24 20:07:33 -0700

    Button component added

--- constants/colors.js
+++ constants/colors.js
@@ -1,6 +1,7 @@
 export default {
-    background: '#E7E9F5',
-    text: '#474444',
-    accent: '#1761A0',
-    primary: '#CF1717'
-}
+  background: "#E7E9F5",
+  text: "#474444",
+  accent: "#1761A0",
+  primary: "#CF1717",
+  white: "#FFF",
+};
--- views/Login/Login.js
+++ views/Login/Login.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
+import AppButton from "../../components/AppButton/AppButton";
 
 export default function Login({ navigation }) {
+  const homeNavigation = () => {
+    navigation.navigate("Home");
+  };
   return (
     <View>
-      <Text onPress={() => navigation.navigate("Home")}>Login</Text>
+      <AppButton title="Login" clickHandler={homeNavigation} />
     </View>
   );
 }
