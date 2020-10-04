6f4f91982cd72ceb148c6cec071b81a290c1cea8
Pooja Kataria <pooja.kataria@sjsu.edu>
2020-10-04 01:40:19 -0700

    Updated Nutrition.js

--- backend/models/Nutrition.js
+++ backend/models/Nutrition.js
@@ -5,27 +5,13 @@ var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 mongoose.set('useCreateIndex', true);
 
-var UserSchema = mongooseBootstrapper({
+var NutritionSchema = mongooseBootstrapper({
 
-    email: {
-        type: String,
-        unique: true
-    },
-    phoneNumber: { type: String },
-    firstName: { type: String },
-    lastName: { type: String },
-    gender: { type: String },
-    type: { type: String },
-    city: { type: String },
-    state: { type: String },
-    country: { type: String },
-    profilePicture: { type: String },
-    signedInFrom: { type: String, enum: ['normal', 'google', 'facebook'] },
-    verified: { type: Boolean },
-    verifiedAt: { type: Date },
-    deleted: { type: Boolean },
-    deletedAt: { type: Date }
+    user: {type: ObjectId, ref:'User'},
+    food: {type: ObjectId, ref:'Food'},
+    calories: { type: Number },
+    createdAt: { type: Date }
 
 });
 
-mongoose.model('User', UserSchema, 'user');
+mongoose.model('Nutrition', NutritionSchema, 'nutrition');
